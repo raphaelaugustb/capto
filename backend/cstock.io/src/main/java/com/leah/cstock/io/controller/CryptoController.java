fea66f23c82b4d4a8d36ab2c4aa07e81bb57fffc
raphaelaugustb <travassosraphael12@gmail.com>
2024-07-06 13:15:35 -0300

    Criando serviço e rota pra cadastra nova criptomoedda com usuário

--- backend/cstock.io/src/main/java/com/leah/cstock/io/controller/CryptoController.java
+++ backend/cstock.io/src/main/java/com/leah/cstock/io/controller/CryptoController.java
@@ -1,12 +1,12 @@
 package com.leah.cstock.io.controller;
 
+import com.leah.cstock.io.dto.request.CryptoRequest;
 import com.leah.cstock.io.dto.response.Crypto.CryptoResponse;
 import com.leah.cstock.io.service.CryptoService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 public class CryptoController {
@@ -19,6 +19,11 @@ public class CryptoController {
     @GetMapping("/crypto")
     public ResponseEntity<CryptoResponse> getCryptoByname(@RequestParam(name = "name") String cryptoId) {
         return ResponseEntity.ok( cryptoService.getCryptoByName(cryptoId));
+    }
+    @PostMapping("{userId}/crypto")
+    public ResponseEntity<CryptoRequest> addUserCryptoOnWallet(@PathVariable UUID userId, @RequestBody CryptoRequest cryptoRequest) {
+        cryptoService.createNewCrypto(cryptoRequest, userId);
+        return  ResponseEntity.ok(cryptoRequest);
     }
 
 }
--- backend/cstock.io/src/main/java/com/leah/cstock/io/service/CryptoService.java
+++ backend/cstock.io/src/main/java/com/leah/cstock/io/service/CryptoService.java
@@ -1,9 +1,17 @@
 package com.leah.cstock.io.service;
 
+import com.leah.cstock.io.dto.request.CryptoRequest;
 import com.leah.cstock.io.dto.response.Crypto.CryptoResponse;
+import com.leah.cstock.io.dto.response.Crypto.Data;
+import com.leah.cstock.io.entity.Crypto;
+import com.leah.cstock.io.entity.Stock;
+import com.leah.cstock.io.entity.User;
 import com.leah.cstock.io.repository.CryptoRepository;
 import com.leah.cstock.io.repository.UserRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
 
 @Service
 public class CryptoService {
@@ -19,8 +27,34 @@ public class CryptoService {
         this.cryptoRepository = cryptoRepository;
         this.userRepository = userRepository;
     }
-
+    public double userCryptoValue(double cryptoAmount, String priceUsd){
+        double userCryptoValuePerAmount = cryptoAmount * Double.parseDouble(priceUsd);
+        return userCryptoValuePerAmount;
+    }
     public CryptoResponse getCryptoByName(String cryptoId) {
      return coinCapService.getCrypto(cryptoId);
+    }
+
+    public void createNewCrypto(CryptoRequest cryptoRequest, UUID userId) {
+        // Pegando as informaçoes da nossa request e da api externa
+        String cryptoName = cryptoRequest.getCryptoName().toLowerCase();
+        double cryptoAmount = cryptoRequest.getCryptoAmount();
+        Data response = coinCapService.getCrypto(cryptoName).data();
+        // Pegando nosso usuário da base de dados
+        User userNotVerificated = userRepository.findById(userId).get();
+        User userVerificated= userService.verifyUser(userNotVerificated);
+        // Criando a entity crypto e setando os campos
+        Crypto crypto = new Crypto();
+        crypto.setCryptoName(response.name());
+        crypto.setCryptoAmount(cryptoAmount);
+        crypto.setCryptoPrice(response.priceUsd());
+        crypto.setTotalUserCryptoValue(userCryptoValue(cryptoAmount,response.priceUsd()));
+        crypto.setSymbol(response.symbol());
+        //Adicionando a criptomoeda na lista do usuário e setando seu balanço
+        userVerificated.getCryptoList().add(crypto);
+        userVerificated.setUserBalance(userService.updateUserBalance(userVerificated.getCryptoList(), userVerificated.getStockList()));
+        //Adicionando a criptomoeda no banco e atualizando o usuário
+        cryptoRepository.save(crypto);
+        userRepository.save(userVerificated);
     }
 }
