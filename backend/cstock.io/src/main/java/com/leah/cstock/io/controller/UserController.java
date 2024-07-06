fe32fee5e061012943e206b26cd5a7f06ab97486
raphaelaugustb <travassosraphael12@gmail.com>
2024-07-05 21:47:48 -0300

    User controller and CRUD routes created

---
+++ backend/cstock.io/src/main/java/com/leah/cstock/io/controller/UserController.java
@@ -0,0 +1,42 @@
+package com.leah.cstock.io.controller;
+
+
+import com.leah.cstock.io.dto.request.UserRequest;
+import com.leah.cstock.io.dto.response.UserResponse;
+import com.leah.cstock.io.service.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+public class UserController {
+    UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/user/register")
+    public ResponseEntity<UserRequest> registerUser(@RequestBody UserRequest userRequest) {
+        userService.createNewUser(userRequest);
+        return ResponseEntity.ok(userRequest);
+    }
+    @GetMapping("{idUser}/user")
+    public  ResponseEntity<UserResponse> getUserInfo(@PathVariable UUID idUser){
+        return ResponseEntity.ok( userService.getUserInfo(idUser));
+    }
+
+    @PutMapping("{idUser}/user")
+    public ResponseEntity<UserRequest> updateUser(@PathVariable UUID idUser, @RequestBody UserRequest userRequest) {
+        userService.updateUserInfo(idUser,userRequest);
+        return ResponseEntity.ok(userRequest);
+    }
+
+    @DeleteMapping("{idUser}/user" )
+    public ResponseEntity<String> deleteUser(@PathVariable UUID idUser) {
+        userService.deleteUserById(idUser);
+        return  ResponseEntity.ok("User deleted successfully");
+    }
+
+}
