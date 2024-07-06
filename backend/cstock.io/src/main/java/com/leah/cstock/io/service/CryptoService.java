88f522f3d8d998e75a20fdb912c0595743b65568
raphaelaugustb <travassosraphael12@gmail.com>
2024-07-06 13:29:23 -0300

    Adicionando verificação de segurança para determinar se o usuário ja tem aquela criptomoeda na sua carteira

--- backend/cstock.io/src/main/java/com/leah/cstock/io/service/CryptoService.java
+++ backend/cstock.io/src/main/java/com/leah/cstock/io/service/CryptoService.java
@@ -36,6 +36,7 @@ public class CryptoService {
     }
 
     public void createNewCrypto(CryptoRequest cryptoRequest, UUID userId) {
+
         // Pegando as informaçoes da nossa request e da api externa
         String cryptoName = cryptoRequest.getCryptoName().toLowerCase();
         double cryptoAmount = cryptoRequest.getCryptoAmount();
@@ -43,6 +44,7 @@ public class CryptoService {
         // Pegando nosso usuário da base de dados
         User userNotVerificated = userRepository.findById(userId).get();
         User userVerificated= userService.verifyUser(userNotVerificated);
+
         // Criando a entity crypto e setando os campos
         Crypto crypto = new Crypto();
         crypto.setCryptoName(response.name());
@@ -50,11 +52,39 @@ public class CryptoService {
         crypto.setCryptoPrice(response.priceUsd());
         crypto.setTotalUserCryptoValue(userCryptoValue(cryptoAmount,response.priceUsd()));
         crypto.setSymbol(response.symbol());
-        //Adicionando a criptomoeda na lista do usuário e setando seu balanço
-        userVerificated.getCryptoList().add(crypto);
-        userVerificated.setUserBalance(userService.updateUserBalance(userVerificated.getCryptoList(), userVerificated.getStockList()));
-        //Adicionando a criptomoeda no banco e atualizando o usuário
-        cryptoRepository.save(crypto);
-        userRepository.save(userVerificated);
+
+        //Verificacao para determinar se a criptomeda ja está listada no repo do usuário
+        boolean isCreateOnUserList = false;
+        for (Crypto cryptoOnList :userVerificated.getCryptoList() ) {
+            if (cryptoOnList.getCryptoName().equals(response.name())) {
+                isCreateOnUserList = true;
+                break;
+            }
+        }
+        // Verificando se ela está listada e executando o método
+        if (isCreateOnUserList == true){
+            for (Crypto cryptoOnList :userVerificated.getCryptoList() ) {
+                if (cryptoOnList.getCryptoName().equals(response.name())) {
+                    // Atualizando a quantidade que o usuário e tem e o valor total
+                    cryptoOnList.setCryptoAmount( cryptoOnList.getCryptoAmount() + cryptoAmount );
+                   cryptoOnList.setTotalUserCryptoValue( cryptoOnList.getTotalUserCryptoValue() + userCryptoValue(cryptoAmount,response.priceUsd()));
+                   cryptoOnList.setCryptoPrice(response.priceUsd());
+                    //Atualizando o saldo do usuário
+                    userVerificated.setUserBalance(userService.updateUserBalance(userVerificated.getCryptoList(), userVerificated.getStockList()));
+                    //Atualizando a criptomoeda no banco e atualizando o usuário
+                    cryptoRepository.save(cryptoOnList);
+                    userRepository.save(userVerificated);
+                }
+            }
+        } else {
+            //Adicionando a criptomoeda na lista do usuário e setando seu balanço
+            userVerificated.getCryptoList().add(crypto);
+            userVerificated.setUserBalance(userService.updateUserBalance(userVerificated.getCryptoList(), userVerificated.getStockList()));
+            //Adicionando a criptomoeda no banco e atualizando o usuário
+            cryptoRepository.save(crypto);
+            userRepository.save(userVerificated);
+        }
+
     }
+    public void deleteCryptoById(UUID idUser, String cryptoId) {}
 }
