6323d1dfc630da44a6c08952c5a69c878e9f1ab8
raphaelaugustb <travassosraphael12@gmail.com>
2024-07-05 21:22:56 -0300

    initial commit

---
+++ backend/cstock.io/src/test/java/com/leah/cstock/io/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leah.cstock.io;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
