c453805f72d03ca39f0499e45098611ea8ace873
raphaelaugustb <travassosraphael12@gmail.com>
2024-07-06 13:14:23 -0300

    Atualizando user service com metodo para atualizar o seu balanço

--- backend/cstock.io/src/main/java/com/leah/cstock/io/service/UserService.java
+++ backend/cstock.io/src/main/java/com/leah/cstock/io/service/UserService.java
@@ -2,12 +2,15 @@ package com.leah.cstock.io.service;
 
 import com.leah.cstock.io.dto.request.UserRequest;
 import com.leah.cstock.io.dto.response.UserResponse;
+import com.leah.cstock.io.entity.Crypto;
+import com.leah.cstock.io.entity.Stock;
 import com.leah.cstock.io.entity.User;
 import com.leah.cstock.io.repository.UserRepository;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.UUID;
 
 
@@ -19,7 +22,21 @@ public class UserService {
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
+    public double updateUserBalance(List<Crypto> cryptoList, List<Stock> stockList) {
+        double userBalance = 0;
+        double cryptoBalance = 0;
+        double stockBalance = 0;
+             for (Crypto crypto : cryptoList){
+                 cryptoBalance += crypto.getTotalUserCryptoValue();
 
+            }
+             for (Stock stock: stockList){
+                stockBalance += stock.getTotalUserStockValue();
+            }
+
+        userBalance = cryptoBalance + stockBalance;
+        return userBalance;
+    }
     public UserRequest verifyUserRequest(UserRequest userRequest) {
         if (userRequest != null){
             return  userRequest;
