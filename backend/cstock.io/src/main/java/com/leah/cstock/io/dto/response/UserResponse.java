0b0b89b896285344c1a4e37d6e38a990f22005c3
raphaelaugustb <travassosraphael12@gmail.com>
2024-07-07 20:55:25 -0300

    refatoring user DTO making it record

--- backend/cstock.io/src/main/java/com/leah/cstock/io/dto/response/UserResponse.java
+++ backend/cstock.io/src/main/java/com/leah/cstock/io/dto/response/UserResponse.java
@@ -1,10 +1,4 @@
 package com.leah.cstock.io.dto.response;
 
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
-
 public record UserResponse(String username, String password, String email, double userBalance) {
 }
