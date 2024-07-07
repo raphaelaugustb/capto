d0ce4d544f1fdc352b8a46e8db7a32e154856d83
raphaelaugustb <travassosraphael12@gmail.com>
2024-07-06 23:06:07 -0300

    DTO for stock response

---
+++ backend/cstock.io/src/main/java/com/leah/cstock/io/dto/response/Stock/Results.java
@@ -0,0 +1,4 @@
+package com.leah.cstock.io.dto.response.Stock;
+
+public record Results(double regularMarketPrice, String symbol, String longName) {
+}
---
+++ backend/cstock.io/src/main/java/com/leah/cstock/io/dto/response/Stock/StockResponse.java
@@ -0,0 +1,6 @@
+package com.leah.cstock.io.dto.response.Stock;
+
+import java.util.List;
+
+public record StockResponse(List<Results> results) {
+}
