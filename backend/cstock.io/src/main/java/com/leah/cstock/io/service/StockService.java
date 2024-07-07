138ab6edabbeab1e60bb16197d69ffd4f9be329e
raphaelaugustb <travassosraphael12@gmail.com>
2024-07-07 20:38:16 -0300

    Refatoring stock for creating his own exceptions

--- backend/cstock.io/src/main/java/com/leah/cstock/io/service/StockService.java
+++ backend/cstock.io/src/main/java/com/leah/cstock/io/service/StockService.java
@@ -1,5 +1,6 @@
 package com.leah.cstock.io.service;
 
+import com.leah.cstock.io.dto.exceptions.Stock.StockNotFoundException;
 import com.leah.cstock.io.dto.request.StockRequest;
 import com.leah.cstock.io.dto.response.Stock.Results;
 import com.leah.cstock.io.dto.response.Stock.StockResponse;
@@ -16,13 +17,12 @@ import java.util.UUID;
 @Service
 public class StockService {
     private final CoinCapService coinCapService;
-    @Value("#{environment.TOKEN}")
-    private String TOKEN;
-
     BrapiService brapiService;
     StockRepository stockRepository;
     UserRepository userRepository;
     UserService userService;
+    @Value("#{environment.TOKEN}")
+    private String TOKEN;
 
     public StockService(BrapiService brapiService, StockRepository stockRepository, UserRepository userRepository, UserService userService, CoinCapService coinCapService) {
         this.brapiService = brapiService;
@@ -36,43 +36,47 @@ public class StockService {
         StockResponse stock = brapiService.getStockById(stockSymbol, TOKEN);
         return stock;
     }
-    public double calcStockUserValue(double amount, double regularMarketPrice){
+
+    public double calcStockUserValue(double amount, double regularMarketPrice) {
         double userStockValue = amount * regularMarketPrice;
         return userStockValue;
     }
-    public List<Stock> getUserStockList(UUID userId){
+
+    public List<Stock> getUserStockList(UUID userId) {
         User userVerified = userService.verifyUser(userId);
         return userVerified.getStockList();
     }
-    public Stock getStockOnUserList(UUID userId, String stockName){
+
+    public Stock getStockOnUserList(UUID userId, String stockName) {
         //Fix bugs to delete next Stock on list
         Stock stockSelected = null;
         User userVerified = userService.verifyUser(userId);
-        for (Stock a: userVerified.getStockList()){
-            if (a.getStockSymbol().equals(stockName)){
+        for (Stock a : userVerified.getStockList()) {
+            if (a.getStockSymbol().equals(stockName)) {
                 stockSelected = a;
                 break;
             }
         }
-        if (stockSelected != null){
+        if (stockSelected != null) {
 
             return stockSelected;
         } else {
-            throw new NullPointerException("Stock not found");
+            throw new StockNotFoundException("Stock not found");
         }
     }
-    public void updateStockOnUserList(UUID userId, StockRequest stockRequest){
+
+    public void updateStockOnUserList(UUID userId, StockRequest stockRequest) {
         String stockName = stockRequest.getStockName();
         double stockAmount = stockRequest.getStockAmount();
         double newRegularMarketPrice = getStock(stockName).results().getFirst().regularMarketPrice();
         User userVerified = userService.verifyUser(userId);
         Stock stockUpdated = null;
-        for (Stock a: userVerified.getStockList()){
-            if (a.getStockSymbol().equals(stockName)){
+        for (Stock a : userVerified.getStockList()) {
+            if (a.getStockSymbol().equals(stockName)) {
                 stockUpdated = a;
             }
         }
-        if (stockUpdated != null){
+        if (stockUpdated != null) {
             stockUpdated.setStockAmount(stockAmount);
             stockUpdated.setStockPrice(String.valueOf(newRegularMarketPrice));
             stockUpdated.setTotalUserStockValue(calcStockUserValue(stockAmount, newRegularMarketPrice));
@@ -81,15 +85,16 @@ public class StockService {
             userRepository.save(userVerified);
 
         } else {
-            throw new NullPointerException("Stock not found");
+            throw new StockNotFoundException("Stock not found");
         }
 
     }
-    public void deleteUserStock(UUID userId, String stockName){
+
+    public void deleteUserStock(UUID userId, String stockName) {
         User userVerified = userService.verifyUser(userId);
         Stock stockRemoved = null;
-        for (Stock a: userVerified.getStockList()){
-            if (a.getStockSymbol().equals(stockName)){
+        for (Stock a : userVerified.getStockList()) {
+            if (a.getStockSymbol().equals(stockName)) {
                 stockRemoved = a;
                 break;
             }
@@ -100,9 +105,10 @@ public class StockService {
             userRepository.save(userVerified);
             stockRepository.deleteById(stockRemoved.getIdStock());
         } else {
-            throw new NullPointerException("Stock not found");
+            throw new StockNotFoundException("Stock not found");
         }
     }
+
     public void createNewStockUser(StockRequest stockRequest, UUID userId) {
         User userVerified = userService.verifyUser(userId);
         boolean stockIsOnUserList = false;
@@ -122,7 +128,7 @@ public class StockService {
             }
         }
 
-        if (stockIsOnUserList){
+        if (stockIsOnUserList) {
             for (Stock a : userVerified.getStockList()) {
                 if (a.getStockSymbol().equals(stockRequest.getStockName())) {
                     a.setStockAmount(stockAmount);
