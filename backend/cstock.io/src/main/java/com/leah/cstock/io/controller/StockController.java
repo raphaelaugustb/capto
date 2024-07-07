f531e63d02cb4eddc7ff042be315bc1b693cc402
raphaelaugustb <travassosraphael12@gmail.com>
2024-07-06 23:49:58 -0300

    create route for get user stock liist

--- backend/cstock.io/src/main/java/com/leah/cstock/io/controller/StockController.java
+++ backend/cstock.io/src/main/java/com/leah/cstock/io/controller/StockController.java
@@ -2,11 +2,13 @@ package com.leah.cstock.io.controller;
 
 import com.leah.cstock.io.dto.request.StockRequest;
 import com.leah.cstock.io.dto.response.Stock.StockResponse;
+import com.leah.cstock.io.entity.Stock;
 import com.leah.cstock.io.service.StockService;
 import feign.Response;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -31,5 +33,9 @@ public class StockController {
     public  ResponseEntity<String> deleteStockOnUserWallet(@PathVariable UUID userId,  @RequestParam("name") String stockName){
         stockService.deleteUserStock(userId, stockName);
         return ResponseEntity.ok("Stock deleted successfully");
+    }
+    @GetMapping("{userId}/stock/list")
+    public ResponseEntity<List<Stock>> getUserStockList(@PathVariable UUID userId){
+        return ResponseEntity.ok(stockService.getUserStockList(userId));
     }
 }
