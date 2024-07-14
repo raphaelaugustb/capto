5f39608076e7bd354c1eec53419e220452633b82
raphaelaugustb <travassosraphael12@gmail.com>
2024-07-13 22:30:01 -0300

    Refactoring http method on Stock Controller for new response

--- backend/cstock.io/src/main/java/com/leah/cstock/io/controller/StockController.java
+++ backend/cstock.io/src/main/java/com/leah/cstock/io/controller/StockController.java
@@ -4,6 +4,7 @@ import com.leah.cstock.io.dto.request.StockRequest;
 import com.leah.cstock.io.dto.response.Stock.StockResponse;
 import com.leah.cstock.io.entity.Stock;
 import com.leah.cstock.io.service.StockService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
@@ -19,30 +20,30 @@ public class StockController {
 
     @GetMapping("/stock")
     public ResponseEntity<StockResponse> getStockById(@RequestParam(name = "name") String stockName){
-        return ResponseEntity.ok(stockService.getStock(stockName));
+        return ResponseEntity.status(HttpStatus.FOUND).body(stockService.getStock(stockName));
     }
     @PostMapping("{userId}/stock")
     public ResponseEntity<StockRequest> createNewStockOnUserWallet(@PathVariable UUID userId,  @RequestBody StockRequest stockRequest){
         stockService.createNewStockUser(stockRequest,userId);
-        return ResponseEntity.ok(stockRequest);
+        return ResponseEntity.status(HttpStatus.CREATED).body(stockRequest);
     }
 
     @DeleteMapping("{userId}/stock")
     public  ResponseEntity<String> deleteStockOnUserWallet(@PathVariable UUID userId,  @RequestParam("name") String stockName){
         stockService.deleteUserStock(userId, stockName);
-        return ResponseEntity.ok("Stock deleted successfully");
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body("Stock deleted successfully");
     }
     @GetMapping("{userId}/stock/list")
     public ResponseEntity<List<Stock>> getUserStockList(@PathVariable UUID userId){
-        return ResponseEntity.ok(stockService.getUserStockList(userId));
+        return ResponseEntity.status(HttpStatus.FOUND).body(stockService.getUserStockList(userId));
     }
     @GetMapping("{userId}/stock")
     public ResponseEntity<Stock> getUserStockByName(@PathVariable UUID userId,  @RequestParam("name") String stockName){
-        return ResponseEntity.ok(stockService.getStockOnUserList(userId, stockName));
+        return ResponseEntity.status(HttpStatus.FOUND).body(stockService.getStockOnUserList(userId, stockName));
     }
     @PutMapping("{userId}/stock")
     public  ResponseEntity<StockRequest> updateStockOnUserWallet(@PathVariable UUID userId, @RequestBody StockRequest stockRequest){
         stockService.updateStockOnUserList(userId, stockRequest);
-        return ResponseEntity.ok(stockRequest);
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body(stockRequest);
     }
 }
